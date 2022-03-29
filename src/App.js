65d200ca6f30cf6512a52f7d33909326ef24fb71
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-03-29 21:52:05 +0800

    Added Private Routing for Logged in Users

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Main } from "./containers";
+import { auth } from "./firebase-config";
 import { Login } from "./pages";
 
 function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState("loading");
+
+    useEffect(() => {
+        const unsub = onAuthStateChanged(auth, (currUser) => {
+            setIsLoggedIn(currUser);
+        });
+        return unsub;
+    }, []);
+
     return (
         <div>
             <Routes>
-                <Route path="/*" element=<Main /> />
-                <Route path="/login" element=<Login /> />
+                <Route
+                    path="/*"
+                    element={isLoggedIn ? <Main /> : <Navigate to="/login" />}
+                />
+                <Route
+                    path="/login"
+                    element={!isLoggedIn ? <Login /> : <Navigate to="/" />}
+                />
             </Routes>
         </div>
     );
