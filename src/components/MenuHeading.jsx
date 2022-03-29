73a93a49ac0223713e6c89a184fec2b254aed0b4
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-03-29 23:47:19 +0800

    Added Popover for Settings

--- src/components/MenuHeading.jsx
+++ src/components/MenuHeading.jsx
@@ -2,8 +2,23 @@ import React, { useState } from "react";
 import { Paper } from "@mui/material";
 import SettingsIcon from "@mui/icons-material/Settings";
 import Avatar from "@mui/material/Avatar";
+import Popover from "@mui/material/Popover";
+import { Box } from "@mui/system";
 
 function MenuHeading(props) {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handlePopover = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClosePopover = () => {
+        setAnchorEl(null);
+    };
+
+    const openPopover = Boolean(anchorEl);
+    const id = openPopover ? "simple-popover" : undefined;
+
     return (
         <Paper
             sx={{
@@ -13,9 +28,64 @@ function MenuHeading(props) {
                 padding: "0 1rem",
             }}
         >
-            <SettingsIcon fontSize="medium" />
+            <SettingsIcon
+                fontSize="medium"
+                aria-describedby={id}
+                onClick={handlePopover}
+                sx={{ cursor: "pointer" }}
+            />
             <h1>Ka-Chat</h1>
             <Avatar />
+
+            {/* Popover for Settings */}
+            <Popover
+                id={id}
+                open={openPopover}
+                anchorEl={anchorEl}
+                onClose={handleClosePopover}
+                anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "center",
+                }}
+                transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                }}
+            >
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                    }}
+                >
+                    <Box
+                        component="h5"
+                        sx={{
+                            padding: "1rem",
+                            cursor: "pointer",
+                            fontWeight: "400",
+                            ":hover": {
+                                backgroundColor: "#E0E0E0",
+                            },
+                        }}
+                    >
+                        Change Avatar
+                    </Box>
+                    <Box
+                        component="h5"
+                        sx={{
+                            padding: "1rem",
+                            cursor: "pointer",
+                            fontWeight: "400",
+                            ":hover": {
+                                backgroundColor: "#E0E0E0",
+                            },
+                        }}
+                    >
+                        Sign Out
+                    </Box>
+                </div>
+            </Popover>
         </Paper>
     );
 }
