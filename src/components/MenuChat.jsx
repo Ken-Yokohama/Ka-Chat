3184561fe2506561f0563415c75104f8f8ee6543
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-03-30 18:04:42 +0800

    Added Logic and Security Rules to create 1-1 chat data ref to chat messages after clicking a user in the new messages

--- src/components/MenuChat.jsx
+++ src/components/MenuChat.jsx
@@ -1,12 +1,66 @@
 import React, { useEffect, useState } from "react";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
+import {
+    addDoc,
+    collection,
+    doc,
+    getDoc,
+    serverTimestamp,
+    setDoc,
+} from "firebase/firestore";
+import { auth, db } from "../firebase-config";
 
 function MenuChat({ registeredUsers }) {
-    const handleOption = (e, value) => {
+    const handleOption = async (e, value) => {
         const getUserObject = registeredUsers.find((e) => e.user == value);
         if (!getUserObject) return;
-        console.log(getUserObject);
+        if (getUserObject.user == auth?.currentUser?.email) return;
+
+        const userChatRefA = doc(
+            db,
+            "users",
+            auth?.currentUser?.uid,
+            "chats",
+            getUserObject?.id
+        );
+
+        const checkIfAlreadyChatted = await getDoc(userChatRefA);
+        if (checkIfAlreadyChatted.exists()) {
+            console.log("Chat Message Exists");
+        } else {
+            const chatsCollectionRef = await collection(db, "chats");
+            // Create Chat in Chats Collection
+            const chatData = await addDoc(chatsCollectionRef, {
+                author: "admin",
+                message: "New Chat Created!",
+                imgSrc: "",
+                timestamp: serverTimestamp(),
+                ids: [auth?.currentUser?.uid, getUserObject?.id],
+            });
+
+            // Add Chats Collection Ref to Nest Chat of Logged In User
+            await setDoc(userChatRefA, {
+                user: getUserObject.user,
+                chatId: chatData.id,
+            });
+
+            const userChatRefB = doc(
+                db,
+                "users",
+                getUserObject?.id,
+                "chats",
+                auth?.currentUser?.uid
+            );
+
+            // Add Chats Collection Ref to Nest Chat of Chatted User
+            await setDoc(userChatRefB, {
+                user: auth?.currentUser?.email,
+                chatId: chatData.id,
+            });
+
+            console.log("Chat Doesn't Exist so New Chat Created!");
+        }
     };
 
     return (
